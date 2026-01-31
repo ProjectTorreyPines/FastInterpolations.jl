42f6b13e9d806c6d2d53bcaad5d71b40151c8fc5
Min-Gu Yoo <mgyoo86@gmail.com>
2026-01-31 11:06:10 -0800

    test: add regression tests for Complex{Int} type promotion in interpolation

--- test/runtests.jl
+++ test/runtests.jl
@@ -44,6 +44,7 @@ else
     include("test_generic_bc.jl")
     include("test_polyfit_bc.jl")
     include("test_bc_structure.jl")
+    include("test_bc_complex_int.jl")
     include("test_type_stability.jl")
     include("test_derivatives.jl")
     include("test_packages_comparison.jl")

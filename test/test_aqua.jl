a439f4da9bb171626cf3d5b646c6ffeeedb8c1b6
Min-Gu Yoo <mgyoo86@gmail.com>
2026-03-10 15:33:02 -0700

    fix: Update false positive count in Aqua test comments

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -3,7 +3,7 @@ using Aqua, Test
 @testset "Aqua.jl" begin
     Aqua.test_all(
         FastInterpolations;
-        # 52 false positives: Julia's has_unbound_tpars flags TypeVars nested
+        # 55 false positives: Julia's has_unbound_tpars flags TypeVars nested
         # inside Vararg (NTuple{N, AbstractVector{Tg}}, NTuple{N, Tg}, etc.).
         # All are internal _-prefixed functions with no runtime impact.
         unbound_args = (broken = true,),
